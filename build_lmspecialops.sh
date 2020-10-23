ddf6e47604c658acd376c14b6193df82eb38dcdd
CrazyT <TheCrazyT@users.noreply.github.com>
2020-10-23 09:32:34 +0200

    problems with tensorflow include dir

--- build_lmspecialops.sh
+++ build_lmspecialops.sh
@@ -4,9 +4,17 @@ conda activate demon
 rm -Rf /demon_v2/lmbspecialops/build 2>/dev/null
 mkdir /demon_v2/lmbspecialops/build
 cd /demon_v2/lmbspecialops/build
+echo pip show tensorflow
+pip show tensorflow
+echo ls /usr/local/envs/demon/lib/python3.6/site-packages/tensorflow
+ls /usr/local/envs/demon/lib/python3.6/site-packages/tensorflow
+echo ls /usr/local/envs/demon/lib/python3.6/dist-packages/tensorflow
+ls /usr/local/envs/demon/lib/python3.6/dist-packages/tensorflow
 #cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 -DTENSORFLOW_INCLUDE_DIR=/usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/include -DTENSORFLOW_FRAMEWORK_LIB=/usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so.1 -DCUDA_ARCH_LIST="7.0" -DCMAKE_LINK_WHAT_YOU_USE=1 -UCMAKE_CXX_STANDARD -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" -DPYTHON_EXECUTABLE=/usr/local/envs/demon/bin/python3.6 ..
-ln -s /usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so.1 /usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so
-cmake -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 -DTENSORFLOW_INCLUDE_DIR=/usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/include -DTENSORFLOW_FRAMEWORK_LIB=/usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so -DCUDA_ARCH_LIST="7.0" -DCMAKE_LINK_WHAT_YOU_USE=1 -UCMAKE_CXX_STANDARD -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" -DPYTHON_EXECUTABLE=/usr/local/envs/demon/bin/python3.6 ..
+#ln -s /usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so.1 /usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so
+#cmake -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 -DTENSORFLOW_INCLUDE_DIR=/usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/include -DTENSORFLOW_FRAMEWORK_LIB=/usr/local/envs/demon/lib/python3.6/site-packages/tensorflow/libtensorflow_framework.so -DCUDA_ARCH_LIST="7.0" -DCMAKE_LINK_WHAT_YOU_USE=1 -UCMAKE_CXX_STANDARD -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" -DPYTHON_EXECUTABLE=/usr/local/envs/demon/bin/python3.6 ..
+ln -s /usr/local/envs/demon/lib/python3.6/dist-packages/tensorflow/libtensorflow_framework.so.1 /usr/local/envs/demon/lib/python3.6/dist-packages/tensorflow/libtensorflow_framework.so
+cmake -DCMAKE_BUILD_TYPE=Release  -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 -DTENSORFLOW_INCLUDE_DIR=/usr/local/envs/demon/lib/python3.6/dist-packages/tensorflow/include -DTENSORFLOW_FRAMEWORK_LIB=/usr/local/envs/demon/lib/python3.6/dist-packages/tensorflow/libtensorflow_framework.so -DCUDA_ARCH_LIST="7.0" -DCMAKE_LINK_WHAT_YOU_USE=1 -UCMAKE_CXX_STANDARD -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" -DPYTHON_EXECUTABLE=/usr/local/envs/demon/bin/python3.6 ..
 if [ "$CORES" != "" ]
 then
 	make VERBOSE=1 -j $CORES
